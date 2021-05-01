f3806fe0fda41aa4fb6273b05bc1711a5b9e76d4
Saalik Lokhandwala <saaliklok@Saaliks-MBP.cable.rcn.com>
2021-05-01 10:04:15 -0400

    completely removed foundation, added bulma, bloomer, and emotion

--- app/javascript/react/components/App.js
+++ app/javascript/react/components/App.js
@@ -1,7 +1,18 @@
 import React from 'react'
+import styled from '@emotion/styled'
+import {Box} from 'bloomer'
+
+const Para = styled.p`
+  color: green;
+`
 
 export const App = (props) => {
-  return (<h1>Make It So React</h1>)
+  return (
+    <Box>
+      <h1 className="title">This is a Bulma Title</h1>
+      <Para>Hello World, from Emotion</Para>
+    </Box>
+  )
 }
 
 export default App
