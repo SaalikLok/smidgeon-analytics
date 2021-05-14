62bf8c405bc0c2765ff940e8adcf6bef2717fe12
Saalik Lokhandwala <saaliklok@Saaliks-MBP.cable.rcn.com>
2021-05-14 10:40:36 -0400

    delete functionality for websites working

---
+++ app/javascript/react/components/website/DeleteButton.js
@@ -0,0 +1,25 @@
+import styled from "@emotion/styled"
+import { Button, Icon } from "bloomer"
+import React from "react"
+
+const ButtonDiv = styled.div`
+	margin-top: 3em;
+`
+
+const DeleteIcon = styled.i`
+	margin: 0 0.3em;
+`
+
+const DeleteButton = (props) => {
+  const { deleteThisWebsite, website } = props
+
+	return (
+		<ButtonDiv>
+			<Button onClick={() => deleteThisWebsite(website)} isColor="light">
+				<DeleteIcon className="fas fa-trash-alt"></DeleteIcon> Delete Website
+			</Button>
+		</ButtonDiv>
+	)
+}
+
+export default DeleteButton
